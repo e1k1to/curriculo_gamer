03185c2881230cf2798679b2cd896beba8049bdb
e1k1to <eikimillian@protonmail.com>
2025-07-10 05:33:02 -0300

    first commit

---
+++ src/test/java/xyz/linuwux/curriculo_gamer/CurriculoGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.linuwux.curriculo_gamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculoGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
