ba58ee9bc887bb5c3ec7d73db5b76b80c12b9962
yuki <eikimillian@protonmail.com>
2025-07-17 00:43:57 -0300

    arrumado cors

--- src/main/java/xyz/linuwux/curriculo_gamer/Application.java
+++ src/main/java/xyz/linuwux/curriculo_gamer/Application.java
@@ -1,7 +1,12 @@
 package xyz.linuwux.curriculo_gamer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@OpenAPIDefinition(servers = { @Server(url="/", description = "Default server url") } )
 
 @SpringBootApplication
 public class Application {
